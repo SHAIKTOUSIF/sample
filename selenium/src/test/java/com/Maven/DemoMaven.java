ccf5db389be8866bff4ef39f6b3e2225251926fb
tousif <tousif@LAPTOP-P04MERIM>
2019-11-14 16:51:11 +0530

    sample

---
+++ selenium/src/test/java/com/Maven/DemoMaven.java
@@ -0,0 +1,11 @@
+package com.Maven;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class DemoMaven {
+@Test
+public void m1() {
+	Reporter.log("Hello world!!  :)Welcome To 'Test Yantra':)", true);
+}
+}
